327be716bd860c1b87bd14d0411390d5fecc46fa
Nikki Bieleveldt <bieleveldtn@gmail.com>
2024-04-08 22:03:41 +0200

    fixed table

---
+++ src/lib/components/Table.svelte
@@ -0,0 +1,47 @@
+<script>
+    let searchTerm = '';
+    let items = [
+        { naam: "John", bier: 5, beschrijving: "Keihard Pilsen", datum: "2022-04-01" },
+        { naam: "Emma", bier: 8, beschrijving: "Laffe Borrel", datum: "2022-04-02" },
+        { naam: "Sophia", bier: 3, beschrijving: "Brak", datum: "2022-04-03" },
+        { naam: "Liam", bier: 6, beschrijving: "Lachu", datum: "2022-04-04" }
+    ];
+    $: filteredItems = items.filter((item) =>
+      item.naam.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+  </script>
+  
+  <div class="flex flex-col mx-5">
+    <div class="mb-4">
+      <input
+        type="text"
+        class="form-input px-4 py-2 border rounded-lg w-1/6 h-[10%]"
+        placeholder="Zoek op naam"
+        bind:value={searchTerm}
+      />
+    </div>
+    <div class="overflow-x-auto relative shadow-md sm:rounded-lg">
+        <table class="w-full text-sm text-left text-gray-500 bg-generalbackground">
+          <thead class="text-xs text-gray-700 uppercase bg-gray-50 bg-widgetaccent">
+            <tr>
+              <th scope="col" class="py-3 px-6">Naam</th>
+              <th scope="col" class="py-3 px-6"># Bier</th>
+              <th scope="col" class="py-3 px-6">Beschrijving</th>
+              <th scope="col" class="py-3 px-6">Datum</th>
+            </tr>
+          </thead>
+          <tbody>
+            {#each filteredItems as item}
+              <tr class="bg-white border-b">
+                <td class="py-4 px-6">{item.naam}</td>
+                <td class="py-4 px-6">{item.bier}</td>
+                <td class="py-4 px-6">{item.beschrijving}</td>
+                <td class="py-4 px-6">{item.datum}</td>
+              </tr>
+            {/each}
+          </tbody>
+        </table>
+      </div>
+      
+  </div>
+  
--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,49 +1,43 @@
 <script>
-	import DataWidget from "$lib/components/DataWidget.svelte";
-    import PrimaryButton from "$lib/components/PrimaryButton.svelte";
+	import DataWidget from '$lib/components/DataWidget.svelte';
+	import PrimaryButton from '$lib/components/PrimaryButton.svelte';
+	import Table from '$lib/components/Table.svelte';
 </script>
 
-<body>
-    <div class="flex flex-col w-full mt-5 h-full">
-        <div class="flex flex-row justify-between mr-4"> 
-            <!-- de bovenste row -->
-            <div class="font-bold text-3xl">Dashboard</div>
-            <div class="flex flex-row gap-4">
-                <!-- buttons -->
-                <div><PrimaryButton text="Nieuwe Voorraad"/></div>
-                <div><PrimaryButton text="Ik heb gezopen"/></div>
-            </div>
-        </div>
-        <div>
-            <!-- de onderste row -->
-            <div class="flex flex-col mt-10 mr-4">
-                <div>
-                    <!-- row met stats -->
-                    <div class="flex flex-row justify-between">
-                        <div class="w-1/6"></div>
-                        <div class="w-1/3 font-bold text-xl flex flex-col">
-                            Huis
-                            <div class="flex flex-row font-normal text-lg pt-4 gap-10">
-                                <div><DataWidget text='Biertjes Over' data='0'/></div>
-                                <div><DataWidget text='Totaal Gedronken' data='0'/></div>
-                            </div>
-                        </div>
-                        <div class="w-1/3 font-bold text-xl">
-                            Persoonlijke Zuipstats
-                            <div class="flex flex-row font-normal text-lg pt-4 gap-10">
-                                <div><DataWidget text='Biertjes Over' data='0'/></div>
-                                <div><DataWidget text='Totaal Gedronken' data='0'/></div>
-                            </div>
-                        </div>
-                        <div class="w-1/6"></div>
+<body class="h-full">
+	<div class="flex flex-col w-full mt-5 h-full">
+		<div class="flex flex-row justify-between mr-4">
+			<!-- de bovenste row -->
+			<div class="font-bold text-3xl">Dashboard</div>
+			<div class="flex flex-row gap-4">
+				<!-- buttons -->
+				<div><PrimaryButton text="Nieuwe Voorraad" /></div>
+				<div><PrimaryButton text="Ik heb gezopen" /></div>
+			</div>
+		</div>
+		<div class="flex flex-col mt-10 mr-4 justify-around h-full">
+            <!-- row met stats -->
+            <div class="flex flex-row">
+                <div class="w-1/6"></div>
+                <div class="w-1/3 font-bold text-xl flex flex-col">
+                    Huis
+                    <div class="flex flex-row font-normal text-lg pt-4 gap-10">
+                        <div><DataWidget text="Biertjes Over" data="0" /></div>
+                        <div><DataWidget text="Totaal Gedronken" data="0" /></div>
                     </div>
                 </div>
-                <div>
-                    <!-- row met tabel -->
-                    tabel
+                <div class="w-1/3 font-bold text-xl">
+                    Persoonlijke Zuipstats
+                    <div class="flex flex-row font-normal text-lg pt-4 gap-10">
+                        <div><DataWidget text="Biertjes Over" data="0" /></div>
+                        <div><DataWidget text="Totaal Gedronken" data="0" /></div>
+                    </div>
                 </div>
+                <div class="w-1/6"></div>
+            </div>
+            <div class="mb-5">
+                <Table />
             </div>
         </div>
-    </div>
-    
+	</div>
 </body>
