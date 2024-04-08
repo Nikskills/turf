3104b0fe03f9e1b9de9024324070aeb9a6d4e752
Nikki Bieleveldt <bieleveldtn@gmail.com>
2024-04-08 20:02:22 +0200

    general layout setup dashboard

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -4,5 +4,29 @@
 </script>
 
 <body>
+    <div class="flex flex-col w-full">
+        <div class="flex flex-row justify-between"> 
+            <!-- de bovenste row -->
+            <div>Dashboard</div>
+            <div class="flex flex-row">
+                <!-- buttons -->
+                <div>Button</div>
+                <div>Button</div>
+            </div>
+        </div>
+        <div>
+            <!-- de onderste row -->
+            <div class="flex flex-col">
+                <div>
+                    <!-- row met stats -->
+                    stats
+                </div>
+                <div>
+                    <!-- row met tabel -->
+                    tabel
+                </div>
+            </div>
+        </div>
+    </div>
     
 </body>
