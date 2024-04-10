eb7cd29f3e07cf8f7552f7f132bf5a52ae119b0a
Nikki Bieleveldt <bieleveldtn@gmail.com>
2024-04-10 14:45:07 +0200

    fixes in responsive navbar

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -3,10 +3,9 @@
     import { page } from '$app/stores';  
     
 </script>
-<!-- this is not working because of the way in which svelte renders-->
 
-<div class="bg-navbarpurple h-screen">
-    <div><a href="/dashboard" class="hover:bg-gray-800 {$page.url.pathname == '/dashboard' ? 'border-2 border-white' : ''}"><img src="logo.png" alt="alt"/></a></div>
+<div class="bg-navbarpurple h-full">
+    <div><a href="/dashboard"><img src="logo.png" alt="alt"/></a></div>
     <div class="flex flex-col justify-evenly h-screen">
         <div><a href="/personal" class="hover:bg-gray-800 "><img src="User_fill.png" class="{$page.url.pathname == '/personal' ? 'border-2 border-white' : ''}" alt="alt"/></a></div>
         <div><a href="/statistics" class="hover:bg-gray-800"><img src="Chart.png" alt="alt" class="{$page.url.pathname == '/statistics' ? 'border-2 border-white' : ''}"/></a></div>
--- src/lib/components/PrimaryButton.svelte
+++ src/lib/components/PrimaryButton.svelte
@@ -1,5 +1,8 @@
 <script>
-    export let text = ''
+    /**
+	 * @type {string}
+	 */
+     export let text
 </script>
 
 <button class="bg-primarybutton hover:bg-primarybuttondark text-black font-bold py-2 px-4 rounded shadow-md transition-transform duration-300 transform hover:-translate-y-1">
