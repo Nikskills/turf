0aa68bfbcad4b429f4991e19552fbafc96975143
Nikki Bieleveldt <bieleveldtn@gmail.com>
2024-05-14 10:14:06 +0200

    fixed overflow issue

--- src/routes/(authenticated)/+layout.svelte
+++ src/routes/(authenticated)/+layout.svelte
@@ -3,10 +3,10 @@
     import "../../app.css"
 </script>
 
-<div class="bg-generalbackground w-100%">
-    <div class="flex flex-row gap-5 ">
-        <div class="w-[5%]"><Navbar /></div>
-        <div class="w-[95%]">
+<div class="bg-generalbackground w-screen min-h-screen">
+    <div class="flex flex-col sm:flex-row gap-5 h-full">
+        <div class="w-full sm:w-1/6 md:w-1/12 lg:w-[5%] h-full"><Navbar /></div>
+        <div class="w-full sm:w-5/6 md:w-11/12 lg:w-[95%] overflow-hidden">
             <slot />
         </div>
     </div>
--- src/routes/(authenticated)/dashboard/+page.svelte
+++ src/routes/(authenticated)/dashboard/+page.svelte
@@ -18,37 +18,37 @@
   ];
 </script>
 
-<div class="flex flex-col w-full mt-5 h-full">
-    <div class="flex flex-row justify-between mr-4">
-        <!-- de bovenste row -->
+<div class="flex flex-col w-full mt-5 h-full flex-wrap">
+    <div class="flex flex-row justify-between mr-4 flex-wrap">
+        <!-- Top row -->
         <div class="font-bold text-3xl">Dashboard</div>
         <div class="flex flex-row gap-4">
-            <!-- buttons -->
+            <!-- Buttons -->
             <div><a href="/drinkingsession"><PrimaryButton text="Nieuwe Voorraad"/></a></div>
             <div><PrimaryButton text="Ik heb gezopen" /></div>
         </div>
     </div>
     <div class="flex flex-col mr-4 justify-around flex-grow">
-        <!-- row met stats -->
-        <div class="flex flex-row">
-            <div class="w-1/6"></div>
-            <div class="w-1/3 font-bold text-xl flex flex-col">
+        <!-- Stats row -->
+        <div class="flex flex-row flex-wrap">
+            <div class="w-full md:w-1/6"></div>
+            <div class="w-full md:w-1/3 font-bold text-xl flex flex-col justify-center">
                 Huis
-                <div class="flex flex-row font-normal text-lg pt-4 gap-10">
-                    <div><DataWidget text="Biertjes Over" databasedata={0}/></div>
-                    <div><DataWidget text="Totaal Gedronken" databasedata={0} /></div>
+                <div class="flex flex-col md:flex-row font-normal text-lg pt-4 gap-4 md:gap-10 flex-wrap sm:mx-2 md:mx-0">
+                    <div class="w-full"><DataWidget text="Biertjes Over" databasedata={0}/></div>
+                    <div class="w-full"><DataWidget text="Totaal Gedronken" databasedata={0} /></div>
                 </div>
             </div>
-            <div class="w-1/3 font-bold text-xl">
+            <div class="w-full md:w-1/3 font-bold text-xl mt-3 md:mt-0">
                 Persoonlijke Zuipstats
-                <div class="flex flex-row font-normal text-lg pt-4 gap-10">
-                    <div><DataWidget text="Biertjes Over" databasedata={0} /></div>
-                    <div><DataWidget text="Totaal Gedronken" databasedata={0} /></div>
+                <div class="flex flex-col md:flex-row font-normal text-lg pt-4 gap-4 md:gap-10 flex-wrap">
+                    <div class="w-full"><DataWidget text="Biertjes Over" databasedata={0} /></div>
+                    <div class="w-full"><DataWidget text="Totaal Gedronken" databasedata={0} /></div>
                 </div>
             </div>
-            <div class="w-1/6"></div>
+            <div class="w-full md:w-1/6"></div>
         </div>
-        <div class="">
+        <div class="md:block hidden">
             <Table {items} {columns}/>
         </div>
     </div>
