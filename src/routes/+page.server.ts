f8b595493a1fbfbafe2068aed0719dc7476d518e
Nikki Bieleveldt <bieleveldtn@gmail.com>
2024-05-23 10:36:55 +0200

    logout works

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,6 +1,34 @@
 // src/routes/+page.server.ts
+import { lucia } from "$lib/server/auth";
+import { fail, redirect } from "@sveltejs/kit";
 
-import { redirect } from '@sveltejs/kit';
-export const load = async () => {
-    throw redirect(302, "/login");
+import type { Actions, PageServerLoad } from "./$types";
+
+export const load: PageServerLoad = async ({ locals }) => {
+	if (!locals.session) {
+        // If not authenticated, redirect to login page
+        throw redirect(302, "/login");
+    }
+
+    // Fetch user data if needed
+    const user = locals.session.userId;
+
+    return {
+        user
+    };
+};
+
+export const actions: Actions = {
+	default: async (event) => {
+		if (!event.locals.session) {
+			return fail(401);
+		}
+		await lucia.invalidateSession(event.locals.session.id);
+		const sessionCookie = lucia.createBlankSessionCookie();
+		event.cookies.set(sessionCookie.name, sessionCookie.value, {
+			path: ".",
+			...sessionCookie.attributes
+		});
+		redirect(302, "/login");
+	}
 };
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,11 @@
 <script lang='ts'>
     import "../app.css";
+    import { enhance } from "$app/forms";
 </script>
 
 
 <body>
-    
+    <form method="post" use:enhance>
+        <button>Sign out</button>
+    </form>
 </body>
