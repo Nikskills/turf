078b4e4d429cfec25b256642f45eca59459515b0
Nikki Bieleveldt <bieleveldtn@gmail.com>
2024-09-30 19:53:22 +0200

    fixed balance page

--- src/routes/balance/+page.server.ts
+++ src/routes/balance/+page.server.ts
@@ -1,14 +1,23 @@
 import { redirect, } from "@sveltejs/kit";
 import type { PageServerLoad } from './$types';
+import prisma from "$lib/prisma";
 
 export const load: PageServerLoad = async ({ locals }) => {
 	if (!locals.session) {
-        // If not authenticated, redirect to login page
         throw redirect(302, "/login");
     }
 
     if (locals.user?.name !== "Huis") {
         throw redirect(302, '/dashboard')
     }
+
+    const users = await prisma.user.findMany({
+        select: {
+            name: true,
+            balance: true
+        }
+    })
+
+    return {users}
 
 }
--- src/routes/balance/+page.svelte
+++ src/routes/balance/+page.svelte
@@ -1,7 +1,39 @@
-<script lang="en">
+<script lang="ts">
+	import TableComp from "$lib/components/TableComp.svelte";
+	import type { PageData } from "./$types";
+    export let data: PageData
+    import '../../app.css'
+
+    const users = data.users
+    let rows = []
+
+    for (let user of users) {
+        console.log(user.name )
+        rows.push({
+            naam: user.name,
+            balans: user.balance
+        })
+    }
+
+    let columns = [
+        {header: 'Naam', key: 'naam'},
+        {header: 'Balans', key: 'balans'}
+    ]
 
 </script>
 
-<div>
-    <h1>Balans</h1>
+<div class="bg-generalbackground min-h-screen">
+    <div class="flex justify-start p-4">
+        <div class="w-[14%]">
+            <button class="rounded-md bg-secondary text-generalbackground py-2 text-sm font-semibold shadow-sm mx-2 my-1 w-full" on:click={() => window.location.href="/kassa"}>Terug</button>
+        </div>
+    </div>
+    
+    <div class="flex flex-col items-center justify-center p-6">
+        <h1 class="text-4xl font-bold text-black mb-8 text-center">Balans</h1>
+
+        <div class="bg-generalbackground rounded-lg p-6 w-1/2 max-w-6xl">
+            <TableComp items={rows} columns={columns} />
+        </div>
+    </div>
 </div>
